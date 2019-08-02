155ff2615c83ffd69195f886f9d69584ad8bc6f5
letuangh1 <45457873+letuangh1@users.noreply.github.com>
2019-08-02 16:16:00 +0700

    demo json
    json is object json

--- Demo-chuoi/demo.js
+++ Demo-chuoi/demo.js
@@ -1,3 +1,4 @@
+/*
 var Ho_ten = "Lê thị bé Nhỏ";
 var So_ky_tu = Ho_ten.length;
 console.log(So_ky_tu);
@@ -11,6 +12,8 @@ console.log(`Ký tự đầu: ${Ky_tu_dau} Ký tự cuối: ${Ky_tu_cuoi}`);
 var Loi_chao= "Xin chào " + Ho_ten;
 console.log(Loi_chao);
 var Hop_le=Ho_ten.trim() !="";
+*/
+/*
 var Dieu_kien=Ho_ten.includes("K");
 var Chi_so=Ho_ten.indexOf("t");
 console.log(`Hợp lệ: ${Hop_le}`)
@@ -24,3 +27,28 @@ console.log(Ho)
 console.log(Ten)
 var Chu_lot=Ho_ten.replace(Ho,"").replace(Ten,"").trim();
 console.log(Chu_lot);
+*/
+//----So nguyen ----
+/*
+var So_nguyen=parseInt("34");
+var Chuoi_So_nguyen=So_nguyen.toString();
+console.log(typeof(So_nguyen))
+console.log(typeof(Chuoi_So_nguyen))
+
+var So_thuc=parseFloat("34.5");
+var Chuoi_So_thuc=So_thuc.toString();
+console.log(typeof(So_thuc))
+console.log(typeof(Chuoi_So_thuc))
+*/
+
+// var Ngay= new Date("4-14-1960")
+// var Chuoi_Ngay=Ngay.toISOString()
+// console.log(typeof(Ngay))
+// console.log(typeof(Chuoi_Ngay))
+
+var Nhan_vien={ "name":"John", "age":30, "car":null };
+console.log(typeof(Nhan_vien)) //json la object json
+var Chuoi_JSON_Nhan_vien=JSON.stringify(Nhan_vien);
+console.log(typeof(Chuoi_JSON_Nhan_vien));
+Nhan_vien=JSON.parse(Chuoi_JSON_Nhan_vien)
+console.log(typeof(Nhan_vien));
